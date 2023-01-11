18f8ce6b1bbf1a8546ffa6a55237bbcad0a6025f
mike_varona <migueluk91@gmail.com>
2023-01-11 16:29:32 +0000

    Working with "ref"s

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
@@ -6,38 +6,34 @@ import ErrorModal from "../UI/ErrorModal";
 import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
     const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault(); // Prevents the page from reloading when the form is submitted (default behavior)
-        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+        const enteredName = nameInputRef.current.value; // This is how you can get the value of an input field using refs
+        const enteredUserAge = ageInputRef.current.value;
+        if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
             setError({
                 title: 'Invalid input',
                 message: 'Please enter a valid name and age (non-empty values).'
             });
             return;
         }
-        if (+enteredAge < 1) {
+        if (+enteredUserAge < 1) {
             setError({
                 title: 'Invalid age',
                 message: 'Please enter a valid age (> 0).'
             });
             return;
         }
-        props.onAddUser(enteredUsername, enteredAge); // This is how you can pass data from a child component to a parent component (using props)
-        setEnteredUsername('');
-        setEnteredAge('');
+        props.onAddUser(enteredName, enteredUserAge); // This is how you can pass data from a child component to a parent component (using props)
+        nameInputRef.current.value = ''; // This is how you can set the value of an input field using refs
+        ageInputRef.current.value = ''; // This is how you can set the value of an input field using refs
     }
 
-    const usernameChangeHandler = (event) => {
-        setEnteredUsername(event.target.value);
-    }
-
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value);
-    }
 
     const errorHandler = () => {
         setError(null);
@@ -56,14 +52,12 @@ const AddUser = (props) => {
                     <label htmlFor="username">Username</label>
                     <input id="username"
                            type="text"
-                           value={enteredUsername}
-                           onChange={usernameChangeHandler}
+                           ref={nameInputRef}
                     />
                     <label htmlFor="age">Age (Years)</label>
                     <input id="age"
                            type="number"
-                           value={enteredAge}
-                           onChange={ageChangeHandler}
+                           ref={ageInputRef}
                     />
                     <Button type="submit">Add User</Button>
                 </form>
